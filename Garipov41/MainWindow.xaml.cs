aff9377a122da548cd558988de9efba270e209a3
shoma <zlonamerenniy@yandex.ru>
2024-01-29 22:13:39 +0500

    Добавьте файлы проекта.

---
+++ Garipov41/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Garipov41
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new ProductPage());
+            manager.MainFrame = MainFrame;
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.GoBack(); 
+
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                Back.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                Back.Visibility = Visibility.Hidden;
+            }
+
+        }
+    }
+}
---
+++ Garipov41/ProductPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Garipov41
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+            var currentProduct = garipov41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
+        }
+    }
+}
